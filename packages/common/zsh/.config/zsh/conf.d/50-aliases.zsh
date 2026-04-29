9930871f3195e712df55d4432684ddc42d157fb5
HAMY <hamy@hamy.xyz>
2026-04-29 16:06:44 -0400

    Feature: Add dangerous Codex alias

--- packages/common/zsh/.config/zsh/conf.d/50-aliases.zsh
+++ packages/common/zsh/.config/zsh/conf.d/50-aliases.zsh
@@ -1,4 +1,5 @@
 # --- Aliases (portable) ---
 alias clauded='claude --dangerously-skip-permissions'
+alias codexd='codex --dangerously-bypass-approvals-and-sandbox'
 alias zls='zellij list-sessions'
 alias za='zellij attach'
